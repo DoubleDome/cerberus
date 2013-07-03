2d99e8b1c4359c0303dce5066b41e90c7f2b614a
DoubleDome <dadalasalsa@gmail.com>
2013-07-02 23:33:35 -0400

    Adding generate task

--- tasks/cerberus.js
+++ tasks/cerberus.js
@@ -47,4 +47,61 @@ module.exports = function(grunt) {
         });
     });
 
+
+    grunt.registerMultiTask('generate', 'Create project template from git repo', function() {
+
+        var options = this.options({
+            force: false
+        });
+
+        var done = this.async();
+        var sys = require('sys');
+        var exec = require('child_process').exec;
+        var command = 'git clone {{git}} {{dest}}';
+        var clean = this.data.clean;
+        if (typeof(clean) != 'object') clean = [clean];
+        clean.unshift('.git*');
+
+        var path = require('path');
+        var destFinal = path.resolve(this.data.dest) + '/';
+
+        var files;
+
+        var name = this.data.git.substring(this.data.git.lastIndexOf('/'));
+        name = name.substring(1, name.lastIndexOf('.git'));
+        grunt.log.write("Cloning '" + name + "' into '" + this.data.dest + "'... ");
+
+        command = command.replace('{{git}}', this.data.git).replace('{{dest}}', this.data.dest);
+
+        function prepare(error, stdout, stderr) {
+            grunt.log.ok();
+            grunt.log.write('Cleaning files... ');
+
+            // Clean
+            for (var i = 0; i < clean.length; i++) {
+                files = grunt.file.expand(destFinal + clean[i]);
+
+                for (var f = 0; f < files.length; f++) {
+                    try {
+                        grunt.file.delete(files[f], options);
+                    } catch (e) {
+                        grunt.log.error();
+                        grunt.verbose.error(e);
+                        grunt.fail.warn('operation failed.');
+                    }
+                }
+            }
+            grunt.log.ok();
+            grunt.log.writeln();
+            grunt.log.success('Template ready!');
+
+            done();
+        }
+
+        if (!grunt.file.exists(destFinal)) {
+            exec(command, prepare);
+        }
+
+    });
+
 };
